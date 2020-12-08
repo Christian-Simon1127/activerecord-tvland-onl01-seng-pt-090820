3a4b7a65aa060c3a53775bcd816e2dfa74d39805
Christian Simon <csimon1127@gmail.com>
2020-12-08 23:38:04 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :show
   
   def say_that_thing_you_say 
-    @actor_id + " always says: " + self.catchphrase
+    Actor.find_by(id: 1) + " always says: " + self.catchphrase
   end 
   
 end
