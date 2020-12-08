fda96502116880aa53c24792e422d91573816131
Christian Simon <csimon1127@gmail.com>
2020-12-08 23:37:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :show
+  belongs_to :network
+  
+  def actors_list
+    self.characters.map do |char|
+      char.actor.full_name
+    end
+    
+  end
   
 end
